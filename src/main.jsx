e4b1b5ffbe10eb47f1b9716d9e32d6931a358bde
Siddharth Kumar <61427404+krsiddharth99@users.noreply.github.com>
2023-03-02 14:50:11 +0530

    file changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store";
 
-store.subscribe(()=>console.log(store.getState()));
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
